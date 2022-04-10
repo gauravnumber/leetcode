349f281fde6c2a600d61b214c73bf8599adf49c4
Gaurav Singh <gauravnumber@gmail.com>
2022-04-10 11:45:50 +0530

    q121 buy stock worked

---
+++ q121.js
@@ -0,0 +1,52 @@
+// function maxProfit(prices: number[]): number {
+//   let diff: number[] = [],
+//     min: number = Math.min(...prices),
+//     pos: number = prices.indexOf(min);
+
+//   while (prices.at(-1) === min) {
+//     prices = prices.slice(0, pos);
+
+//     min = Math.min(...prices);
+//     pos = prices.indexOf(min);
+//   }
+
+//   for (let i = pos + 1, il = prices.length; i < il; i++) {
+//     diff.push(prices[i] - prices[pos]);
+//   }
+
+//   return diff.length ? Math.max(...diff) : 0;
+// }
+
+// interface diffType {}
+
+// function maxProfit(prices: number[]): number {
+function maxProfit(prices) {
+  let diff = {}, value, max = -Infinity
+
+  for (let i = 0, il = prices.length; i < il; i++) {
+    let price = prices[i];
+    diff[price] = [];
+
+    for (let j = i + 1; j < il; j++) {
+      diff[price].push(prices[j] - price);
+    }
+  }
+
+  for (const price in diff) {
+    if (Object.prototype.hasOwnProperty.call(diff, price)) {
+      const element = diff[price];
+
+      value = Math.max(...element)
+
+      if (max < value) {
+        max = value;
+      }
+    }
+  }
+
+  return max < 0 ? 0 : max;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // => 5
+console.log(maxProfit([7, 6, 5, 4, 3, 2, 1])); // => 0
+console.log(maxProfit([2, 4, 1])); // => 2
