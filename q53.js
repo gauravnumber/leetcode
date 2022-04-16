4301d2314de58b3adaf516c3378b3e8587b1a3ea
Gaurav Singh <gauravnumber@gmail.com>
2022-04-16 10:50:42 +0530

    q53 worked

--- q53.js
+++ q53.js
@@ -39,5 +39,19 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
+  let maxSub = nums[0], curSum = 0
 
+  for (let i = 0; i < nums.length; i++) {
+    if (curSum < 0) curSum = 0
+
+    curSum += nums[i]
+    maxSub = Math.max(maxSub, curSum)
+  }
+
+  return maxSub
 };
+
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // => 6
+console.log(maxSubArray([1])) // => 1
+console.log(maxSubArray([5, 4, -1, 7, 8])) // => 23
