b54bd895d522125f91973f55b63559677c2d92b9
Gaurav Singh <gauravnumber@gmail.com>
2022-04-17 08:39:22 +0530

    q152 worked

--- q152.js
+++ q152.js
@@ -1,12 +1,14 @@
-const maxSubArray = nums => {
+const maxProduct = nums => {
   let res = Math.max(...nums), curMin = 1, curMax = 1, n
-
 
   for (let i = 0; i < nums.length; i++) {
     n = nums[i]
 
-    curMax = Math.max(curMax * n, curMin * n, n)
-    curMin = Math.min(curMax * n, curMin * n, n)
+    let max = curMax * n
+    let min = curMin * n
+
+    curMax = Math.max(max, min, n)
+    curMin = Math.min(max, min, n)
 
     res = Math.max(res, curMax)
   }
@@ -15,6 +17,14 @@ const maxSubArray = nums => {
 }
 
 
-console.log(maxSubArray([2, 3, -2, 4]))
-console.log(maxSubArray([-2, 0, -1]))
-console.log(maxSubArray([-3, -1, -1]))
+console.log(maxProduct([2, 3, -2, 4])) // => 6
+console.log(maxProduct([-2, 0, -1])) // => 0
+console.log(maxProduct([-3, -1, -1])) // => 3
+console.log(maxProduct([-4, -3, -2])) // => 12
+
+// Runtime: 69 ms, faster than 81.60% of JavaScript online submissions for Maximum Product Subarray.
+// Memory Usage: 42.9 MB, less than 42.74% of JavaScript online submissions for Maximum Product Subarray.
+// Next challenges:
+// House Robber
+// Maximum Product of Three Numbers
+// Subarray Product Less Than K
