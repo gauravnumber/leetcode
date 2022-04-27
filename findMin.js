89134a6f62bbacf6c6fe1ae56a43c8d4f340be09
Gaurav Singh <gauravnumber@gmail.com>
2022-04-27 11:34:45 +0530

    findmin

---
+++ findMin.js
@@ -0,0 +1,25 @@
+function findMin(nums) {
+  let l = 0, r = nums.length - 1, res = nums[0], m
+
+  while (l <= r) {
+    if (nums[l] < nums[r]) {
+      res = nums[l]
+      break
+    }
+
+    m = Math.floor((l + r) / 2)
+
+    if (nums[m] >= nums[l]) {
+      l = m + 1
+    }
+    else {
+      r = m - 1
+    }
+  }
+
+  return res
+}
+
+console.log(findMin([3, 4, 5, 1, 2]))
+console.log(findMin([4, 5, 6, 7, 0, 1, 2]))
+console.log(findMin([11, 13, 15, 17]))
