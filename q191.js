c88fde15c172b33c1cfbb7fff06c8906c71a2f93
Gaurav Singh <gauravnumber@gmail.com>
2022-04-29 19:36:32 +0530

    humming weight

--- q191.js
+++ q191.js
@@ -1,3 +1,24 @@
 function hummingWeight(n) {
+  let res = 0
 
+  while (n) {
+    n &= n - 1
+    res++
+  }
+
+  return res
 }
+
+// function hummingWeight(n) {
+//   let res = 0
+
+//   while (n) {
+//     res += n % 2
+//     n = n >> 1
+//   }
+
+//   return res
+// }
+
+console.log(hummingWeight(0000000001011))
+console.log(hummingWeight(000000000010100001011))
