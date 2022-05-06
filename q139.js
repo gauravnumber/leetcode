f614486e4362cf875d8bcae0e11febea8e37b154
Gaurav Singh <gauravnumber@gmail.com>
2022-05-06 16:17:52 +0530

    139 added

---
+++ q139.js
@@ -0,0 +1,21 @@
+function wordBreak(s, wordDict) {
+  let dp = Array(s.length + 1).fill(false)
+  dp[s.length] = true
+
+  for (let i = s.length - 1; i > -1; i--) {
+    for (let wi = 0; wi < wordDict.length; wi++) {
+      let w = wordDict[wi]
+
+      if (i + w.length <= s.length && s.substring(i, i + w.length) == w) {
+        dp[i] = dp[i + w.length]
+      }
+
+      if (dp[i]) break
+    }
+  }
+
+  return dp[0]
+}
+
+console.log(wordBreak("leetcode", ["leet", "code"]))
+console.log(wordBreak("leetcode", ["neet", "code"]))
