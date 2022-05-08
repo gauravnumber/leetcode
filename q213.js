92d48c10713638e0d2678ec490dc60af7d69ca84
Gaurav Singh <gauravnumber@gmail.com>
2022-05-08 16:28:18 +0530

    house robber 2 done

---
+++ q213.js
@@ -0,0 +1,20 @@
+const helper = nums => {
+  let rob1, rob2, n, newRob
+  rob1 = rob2 = 0
+
+  for (let i = 0; i < nums.length; i++) {
+    n = nums[i]
+
+    newRob = Math.max(rob1 + n, rob2)
+    rob1 = rob2
+    rob2 = newRob
+  }
+
+  return rob2
+}
+
+const rob = nums => {
+  return Math.max(helper(nums.slice(1)), helper(nums.slice(-1)))
+}
+
+console.log(rob([2, 3, 2]))
