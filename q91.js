823e36480bca5942d72b45eda87331f149c243ac
Gaurav Singh <gauravnumber@gmail.com>
2022-05-08 18:23:30 +0530

    number decoding

--- q91.js
+++ q91.js
@@ -1,15 +1,6 @@
 const numDecodings = (s) => {
-  let len = s.length
-  let dp = {
-    len: 1
-  }
-
-  // len.bind(dp)
-
-  // console.log(`/[0123456]/.test(s)`, /[0123456]/.test(s))
-
-  // console.log(`s[0]`, s[0])
-  // console.log(`s[1]`, s[1])
+  let dp = {}
+  dp[s.length] = 1
 
   for (let i = s.length - 1; i > -1; i--) {
     console.log(`dp`, dp)
@@ -23,7 +14,6 @@ const numDecodings = (s) => {
 
     }
 
-    // if (i + 1 < s.length && (s[i] == "1" || s[i] == "2" && s[i + 1] in "0123456")) {
     if (i + 1 < s.length && (s[i] == "1" || s[i] == "2" && /[0123456]/.test(s + 1))) {
       dp[i] += dp[i + 2]
     }
@@ -32,4 +22,4 @@ const numDecodings = (s) => {
   return dp[0]
 }
 
-console.log(numDecodings("12"))
+console.log(numDecodings("12")) // => 2
