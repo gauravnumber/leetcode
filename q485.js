2b771aeddce8057027a74cd6abd58a9115a57da3
Gaurav Singh <gauravnumber@gmail.com>
2022-05-29 20:26:01 +0530

    max consecutive ones

---
+++ q485.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let curSum = 0,maxS = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            curSum = 0
+        } else {
+            
+        curSum++
+        }
+        
+        
+        maxS = Math.max(maxS, curSum)
+    }
+    
+    return maxS
+};
+
