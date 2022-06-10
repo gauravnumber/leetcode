98d5922a395c1bac8b0c3b1a8df5b31256537043
Gaurav Singh <gauravnumber@gmail.com>
2022-06-10 20:40:58 +0530

    add

--- q33.js
+++ q33.js
@@ -23,6 +23,6 @@ function search(nums, target) {
   return -1
 }
 
-console.log(search([4, 5, 6, 7, 0, 1, 2], 0))
-console.log(search([4, 5, 6, 7, 0, 1, 2], 3))
-console.log(search([-1], 0))
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0)) //  =>  4
+console.log(search([4, 5, 6, 7, 0, 1, 2], 3)) //  =>  -1
+console.log(search([-1], 0)) //  =>  -1
