7c9d9be65ebd3d1d520a5e8d512017b921c2d983
Gaurav Singh <gauravnumber@gmail.com>
2022-06-17 19:38:38 +0530

    trie intro

--- q208.js
+++ q208.js
@@ -1,60 +1,60 @@
 class TrieNode {
   constructor() {
-    this.children = {}
-    this.endOfWord = false
+    this.children = {};
+    this.endOfWord = false;
   }
 }
 
 class Trie {
   constructor() {
-    this.root = new TrieNode()
+    this.root = new TrieNode();
   }
 
   insert(word) {
-    let cur = this.root
+    let cur = this.root;
 
     for (let c of word) {
       if (!cur.children.hasOwnProperty(c)) {
-        cur.children[c] = new TrieNode()
+        cur.children[c] = new TrieNode();
       }
 
-      cur = cur.children[c]
+      cur = cur.children[c];
     }
 
-    cur.endOfWord = true
+    cur.endOfWord = true;
   }
 
   search(word) {
-    let cur = this.root
+    let cur = this.root;
 
     for (let c of word) {
       if (!cur.children.hasOwnProperty(c)) {
-        return false
+        return false;
       }
 
-      cur = cur.children[c]
+      cur = cur.children[c];
     }
 
-    return cur.endOfWord
+    return cur.endOfWord;
   }
 
   startWith(word) {
-    let cur = this.root
+    let cur = this.root;
 
     for (let c of word) {
       if (!cur.children.hasOwnProperty(c)) {
-        return false
+        return false;
       }
 
-      cur = cur.children[c]
+      cur = cur.children[c];
     }
 
-    return true
+    return true;
   }
 }
 
-let trie = new Trie()
-trie.insert("apple")
-trie.insert("pine")
-console.log(trie.search("apple"))
-console.log(trie.startWith("pine"))
+let trie = new Trie();
+trie.insert("apple");
+trie.insert("pine");
+console.log(trie.search("apple"));
+console.log(trie.startWith("pine"));
