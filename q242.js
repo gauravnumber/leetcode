437934c65f9278600013ba53176e5f4f983ae487
Gaurav Singh <gauravnumber@gmail.com>
2022-06-21 19:31:19 +0530

    add

--- q242.js
+++ q242.js
@@ -7,20 +7,20 @@
 //   return s.split("").sort().join("") === t.split("").sort().join("");
 // };
 
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false
-    const hashMap = {}
-    
-    for (let c of s) {
-        hashMap[c] = (hashMap[c] || 0) + 1;
-    }
-    
-    for (let c of t) {
-        if (!hashMap[c]) return false
-        hashMap[c]--
-    }
-    
-    return true
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  const hashMap = {};
+
+  for (let c of s) {
+    hashMap[c] = (hashMap[c] || 0) + 1;
+  }
+
+  for (let c of t) {
+    if (!hashMap[c]) return false;
+    hashMap[c]--;
+  }
+
+  return true;
 };
 
 console.log(isAnagram("anagram", "nagaram")); // => true
