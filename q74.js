b797410f435f3d902616cdb62f8840b356ea95be
Gaurav Singh <gauravnumber@gmail.com>
2022-07-03 18:53:41 +0530

    add

--- q74.js
+++ q74.js
@@ -3,12 +3,35 @@
  * @param {number} target
  * @return {boolean}
  */
+// var searchMatrix = function (matrix, target) {
+//   for (let i = 0; i < matrix.length; i++) {
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       if (matrix[i][j] === target) {
+//         return true;
+//       }
+//     }
+//   }
+
+//   return false;
+// };
+
 var searchMatrix = function (matrix, target) {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === target) {
-        return true;
-      }
+  let row_num = matrix.length;
+  let col_num = matrix[0].length;
+
+  let begin = 0, end = row_num * col_num - 1;
+
+  while (begin <= end) {
+    let mid = Math.trunc((begin + end) / 2);
+    let mid_value = matrix[Math.trunc(mid / col_num)][mid % col_num];
+
+    if (mid_value == target) {
+      return true;
+    } else if (mid_value < target) {
+      //Should move a bit further, otherwise dead loop.
+      begin = mid + 1;
+    } else {
+      end = mid - 1;
     }
   }
 
