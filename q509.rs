a2cdbb53da735970a8ce824b9e31ce2741495fb3
Gaurav Singh <gauravnumber@gmail.com>
2022-07-06 19:10:04 +0530

    add

--- q509.rs
+++ q509.rs
@@ -1,13 +1,10 @@
-// not working
-fn fib(n: i32) {
-    // let m = n + 2;
-    let mut f:[i32; 7];
-    // let mut f: Vec<i32> = vec![];
+fn fib(n: usize) -> usize {
+    let mut f = vec![0; n + 2];
 
     f[0] = 0;
     f[1] = 1;
 
-    let mut i = 2;
+    let mut i: usize = 2;
 
     while i <= n {
         f[i] = f[i - 1] + f[i - 2];
@@ -18,7 +15,7 @@ fn fib(n: i32) {
 }
 
 fn main() {
-    let res = fib(5);
+    let res = fib(12);
 
     println!("{:?}", res);
 }
