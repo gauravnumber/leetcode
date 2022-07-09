1aac0b645eaf4e88efe5ac7a2a0dd5bac6abb1c5
Gaurav Singh <gauravnumber@gmail.com>
2022-07-09 16:30:07 +0530

    add

--- codewars/digital_root.js
+++ codewars/digital_root.js
@@ -1,19 +1,18 @@
 function digital_root(n) {
-  n = String(n)
-  
-  let sum = n.split("").reduce((a, b) => Number(a) + Number(b))
-  sum = String(sum)
+  n = String(n);
 
-  let len = sum.length 
-  
-  if (len > 1) 
-    return digital_root(sum)
+  let sum = n.split("").reduce((a, b) => Number(a) + Number(b));
+  sum = String(sum);
+
+  let len = sum.length;
+
+  if (len > 1) {
+    return digital_root(sum);
+  }
 
   return Number(sum);
-
 }
 
-
-console.log(digital_root(41)) // 3
-console.log(digital_root(7)) // 7
-console.log(digital_root(12331)) // 1
+console.log(digital_root(41)); // 3
+console.log(digital_root(7)); // 7
+console.log(digital_root(12331)); // 1
--- codewars/isSquare.js
+++ codewars/isSquare.js
@@ -1,18 +1,19 @@
-var isSquare = function(n){
-  const sqrt = Math.sqrt(n)
+var isSquare = function (n) {
+  const sqrt = Math.sqrt(n);
 
-  if (Number.isInteger(sqrt))
-    return true
-  return false; 
-}
+  if (Number.isInteger(sqrt)) {
+    return true;
+  }
+  return false;
+};
 
 // function isSquare(n) {
 //   return Math.sqrt(n) % 1 === 0;
 // }
 
-console.log(isSquare(-1)) // false
-console.log(isSquare(0)) // true
-console.log(isSquare(3)) // false
-console.log(isSquare(4)) // true
-console.log(isSquare(25)) // true
-console.log(isSquare(26)) // false
+console.log(isSquare(-1)); // false
+console.log(isSquare(0)); // true
+console.log(isSquare(3)); // false
+console.log(isSquare(4)); // true
+console.log(isSquare(25)); // true
+console.log(isSquare(26)); // false
